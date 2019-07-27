013699ae769d471341f14a4f18a1b420b26442b4
aman-goibibo <aman.garg@go-mmt.com>
2019-07-27 16:35:14 +0530

    abq

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 
 
   $(document).ready(function () {
-      console.log(location.href.lat)
-      let lat = new URLSearchParams(window.location.search)
-      let long = new URLSearchParams(window.location.long)
+    //   console.log(location.href.lat)
+    //   let lat = new URLSearchParams(window.location.search)
+    //   let long = new URLSearchParams(window.location.long)
+      function param(name) {
+        return (location.search.split(name + '=')[1] || '').split('&')[0];
+    }
+
+    let lat = param(lat);
 
     console.log(lat);
 
