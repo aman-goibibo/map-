a6f261abc4c16005129a68bfe9f300a431678584
aman-goibibo <aman.garg@go-mmt.com>
2019-07-27 16:46:18 +0530

    abq

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
+var getparams = {};
 
-
-  $(document).ready(function () {
+$(document).ready(function () {
     //   console.log(location.href.lat)
     //   let lat = new URLSearchParams(window.location.search)
     //   let long = new URLSearchParams(window.location.long)
-      function param(name) {
-        return (location.search.split(name + '=')[1] || '').split('&')[0];
-    }
+    //   function param(name) {
+    //     return (location.search.split(name + '=')[1] || '').split('&')[0];
+    // }
 
-    let lat = param(lat);
+    getparams = decodeURI(window.location.search)
+        .replace('?', '')
+        .split('&')
+        .map(param => param.split('='))
+        .reduce((values, [key, value]) => {
+            values[key] = value
+            return values
+        }, {})
+    
 
-    console.log(lat);
+})
 
-  })
+function initialize() {
+    console.log(getparams.lat);
+    var fenway = { lat: getparams.lat, lng: getparams.long };
 
-  function initialize() {
-    var fenway = {lat: 42.345573, lng: -71.098326};
-  
     var panorama = new google.maps.StreetViewPanorama(
         document.getElementById('pano'), {
-          position: fenway,
-          pov: {
-            heading: 34,
-            pitch: 10
-          }
+            position: fenway,
+            pov: {
+                heading: 34,
+                pitch: 10
+            }
         });
-        
-        console.log(panorama)
-  }
+
+    console.log(panorama)
+}
